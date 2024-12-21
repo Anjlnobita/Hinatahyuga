0d296aa5e0fefd19745a297a22516a6aff55c2c6
Anjlnobita <jiteshkumar7200@gmail.com>
2024-12-22 02:03:10 +0530

    Update __init__.py

--- Hinatahyuga/__init__.py
+++ Hinatahyuga/__init__.py
@@ -151,9 +151,9 @@ DEV_USERS.add(6777860063)
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("Dazai", API_ID, API_HASH)
+telethn = TelegramClient("hinata", API_ID, API_HASH)
 
-pbot = Client("DazaiRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pbot = Client("Hinatahyuga", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 aiohttpsession = ClientSession()
 
