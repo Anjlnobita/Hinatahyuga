ac1e6dc2f67c9dc68c015474b6058b556b1e9ff4
Anjlnobita <jiteshkumar7200@gmail.com>
2024-12-22 01:57:04 +0530

    Update msg_types.py

--- Hinatahyuga/modules/helper_funcs/msg_types.py
+++ Hinatahyuga/modules/helper_funcs/msg_types.py
@@ -2,7 +2,7 @@ from enum import IntEnum, unique
 
 from telegram import Message
 
-from DazaiRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from Hinatahyuga.modules.helper_funcs.string_handling import button_markdown_parser
 
 
 @unique
