917d8704b8f84e48f9374539dad7e72c190eac5b
Anjlnobita <jiteshkumar7200@gmail.com>
2024-12-21 23:26:06 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop DazaiRobot
-net start DazaiRobot
+net stop Hinatahyuga
+net start Hinatahyuga
