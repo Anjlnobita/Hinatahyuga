77f0cfcb21b5d167b9513baf6974ea8f57be56fe
Anjlnobita <jiteshkumar7200@gmail.com>
2024-12-22 11:00:17 +0530

    Update chatstatus.py

--- Hinatahyuga/modules/helper_funcs/telethn/chatstatus.py
+++ Hinatahyuga/modules/helper_funcs/telethn/chatstatus.py
@@ -1,7 +1,7 @@
 from telethon.tl.types import ChannelParticipantsAdmins
 
-from DazaiRobot import DRAGONS
-from DazaiRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from Hinatahyuga import DRAGONS
+from Hinatahyuga.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
 
 
 async def user_is_ban_protected(user_id: int, message):
