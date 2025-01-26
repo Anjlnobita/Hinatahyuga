ea3acaf70946b8e26074b3b0db82365bd13a2119
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-27 02:22:24 +0530

    Update __init__.py

--- Hinatahyuga/__init__.py
+++ Hinatahyuga/__init__.py
@@ -2,156 +2,89 @@ import logging
 import os
 import sys
 import time
+import aiohttp
+from aiohttp import ClientSession
 
 import telegram.ext as tg
 from pyrogram import Client, errors
 from telethon import TelegramClient
+import config 
 
-StartTime = time.time()
 
 # enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
     level=logging.INFO,
 )
 
-logging.getLogger("apscheduler").setLevel(logging.ERROR)
-logging.getLogger("telethon").setLevel(logging.ERROR)
-logging.getLogger("pymongo").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-# if version < 3.6, stop bot.
-if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    LOGGER.error(
-        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
-    )
-    quit(1)
 
-ENV = bool(os.environ.get("ENV", False))
 
-if ENV:
-    API_ID = int(os.environ.get("API_ID", None))
-    API_HASH = os.environ.get("API_HASH", None)
-    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
-    DB_URI = os.environ.get("MONGO_DB_URI")
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
-    INFOPIC = bool(os.environ.get("INFOPIC", "True"))
-    LOAD = os.environ.get("LOAD", "").split()
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
-    NO_LOAD = os.environ.get("NO_LOAD", "").split()
-    START_IMG = os.environ.get(
-        "START_IMG", "https://telegra.ph/file/40eb1ed850cdea274693e.jpg"
-    )
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "DevilsHeavenMF")
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
-    TOKEN = os.environ.get("TOKEN", None)
-    WORKERS = int(os.environ.get("WORKERS", 8))
 
-    try:
-        OWNER_ID = int(os.environ.get("OWNER_ID", None))
-    except ValueError:
-        raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
-    try:
-        BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
 
-    try:
-        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
 
-    try:
-        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
 
-    try:
-        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
-    except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
 
-    try:
-        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
 
-else:
-    from Hinatahyuga.config import Development as Config
 
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
-    ALLOW_CHATS = Config.ALLOW_CHATS
-    ALLOW_EXCL = Config.ALLOW_EXCL
-    DB_URI = Config.MONGO_DB_URI
-    DEL_CMDS = Config.DEL_CMDS
-    EVENT_LOGS = Config.EVENT_LOGS
-    INFOPIC = Config.INFOPIC
-    LOAD = Config.LOAD
-    MONGO_DB_URI = Config.MONGO_DB_URI
-    NO_LOAD = Config.NO_LOAD
-    START_IMG = Config.START_IMG
-    STRICT_GBAN = Config.STRICT_GBAN
-    SUPPORT_CHAT = Config.SUPPORT_CHAT
-    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
-    TOKEN = Config.TOKEN
-    WORKERS = Config.WORKERS
+import config
 
-    try:
-        OWNER_ID = int(Config.OWNER_ID)
-    except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
+TOKEN = config.BOT_TOKEN
 
-    try:
-        BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
+try:
+    OWNER_ID = int(config.OWNER_ID)
+except ValueError:
+    raise Exception("Your OWNER_ID variable is not a valid integer.")
 
-    try:
-        DRAGONS = set(int(x) for x in Config.DRAGONS or [])
-        DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
 
-    try:
-        DEMONS = set(int(x) for x in Config.DEMONS or [])
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
+try:
+    DRAGONS = set(int(x) for x in config.DRAGONS or [])
+    DEV_USERS = set(int(x) for x in config.DEV_USERS or [])
+except ValueError:
+    raise Exception("Your sudo or dev users list does not contain valid integers.")
 
-    try:
-        TIGERS = set(int(x) for x in Config.TIGERS or [])
-    except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
+try:
+    DEMONS = set(int(x) for x in config.DEMONS or [])
+except ValueError:
+    raise Exception("Your support users list does not contain valid integers.")
 
-    try:
-        WOLVES = set(int(x) for x in Config.WOLVES or [])
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
+try:
+    WOLVES = set(int(x) for x in config.WOLVES or [])
+except ValueError:
+    raise Exception("Your whitelisted users list does not contain valid integers.")
+
+try:
+    TIGERS = set(int(x) for x in config.TIGERS or [])
+except ValueError:
+    raise Exception("Your tiger users list does not contain valid integers.")
+
+
+
+MONGO_DB_URI = config.MONGO_DB_URI
+DEL_CMDS = config.DEL_CMDS
+STRICT_GBAN = config.STRICT_GBAN
+ALLOW_EXCL = config.ALLOW_EXCL
+SUPPORT_CHAT = config.SUPPORT_CHAT
+OWNER_ID = config.OWNER_ID
+BOT_USERNAME "jane_doe_cute_bot"
+
+
+
+
+
+
 
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(1356469075)
+DEV_USERS.add(OWNER_ID)
+DEV_USERS.add(OWNER_ID)
+DEV_USERS.add(OWNER_ID)
 
 
-updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("hinata", API_ID, API_HASH)
-
-pbot = Client("Hinatahyuga", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
-dispatcher = updater.dispatcher
-
-print("[INFO]: Getting Bot Info...")
-BOT_ID = dispatcher.bot.id
-BOT_NAME = dispatcher.bot.first_name
-BOT_USERNAME = dispatcher.bot.username
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
@@ -159,14 +92,40 @@ WOLVES = list(WOLVES)
 DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
-# Load at end to ensure all prev variables have been set
-from Hinatahyuga.modules.helper_funcs.handlers import (
-    CustomCommandHandler,
-    CustomMessageHandler,
-    CustomRegexHandler,
-)
 
-# make sure the regex handler can take extra kwargs
-tg.RegexHandler = CustomRegexHandler
-tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+LOAD = []
+NO_LOAD = []
+API_ID = 20650066
+API_HASH = "7a4f8ed638f1369a40693574c2835217"
+TOKEN = "7970949227:AAHHqi_yNrnpN4w0criBvVri_YX6D1BmUKg"
+
+
+WORKERS = 8
+updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+telethn = TelegramClient("Noi", API_ID, API_HASH)
+pbot = Client("NoinoiRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+dispatcher = updater.dispatcher
+print("[INFO]: INITIALIZING AIOHTTP SESSION")
+
+
+print("[INFO]: Getting Bot Info...")
+BOT_ID = dispatcher.bot.id
+BOT_NAME = dispatcher.bot.first_name
+BOT_USERNAME = dispatcher.bot.username
